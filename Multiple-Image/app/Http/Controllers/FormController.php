9382dfca30a5eba3797babfd5395480d2cfc2857
Tamilarasan Annamalai <54737727+tamilarasana@users.noreply.github.com>
2021-11-11 10:08:11 +0530

    Multipe image upload

---
+++ Multiple-Image/app/Http/Controllers/FormController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Form;
+
+class FormController extends Controller
+{
+      public function create()
+    {
+        //
+        return view('create');
+    }
+
+       public function store(Request $request)
+
+    {
+
+        $this->validate($request, [
+
+                'filename' => 'required',
+                'filename.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+
+        ]);
+        
+        if($request->hasfile('filename'))
+         {
+
+            foreach($request->file('filename') as $image)
+            {
+                $name=$image->getClientOriginalName();
+                $image->move(public_path().'/images/', $name);  
+                $data[] = $name;  
+            }
+         }
+
+         $form= new Form();
+         $form->filename=json_encode($data);
+         
+        
+        $form->save();
+
+        return back()->with('success', 'Your images has been successfully');
+    }
+}
